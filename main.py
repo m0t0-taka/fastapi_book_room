06169ff0a5e6aa0b11c337c2483865114bb1c660
m0t0-taka <tkg.8.mthr@gmail.com>
2022-11-23 14:11:07 +0900

    schema作成

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-import datetime
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
-
-
-class Booking(BaseModel):
-    booking_id: int
-    user_id: int
-    room_id: int
-    booked_num: int
-    start_at: datetime.datetime
-    end_at: datetime.datetime
-
-
-class User(BaseModel):
-    user_id: int
-    user_name: str = Field(max_length=12)
-
-
-class Room(BaseModel):
-    room_id: int
-    room_name: str = Field(max_length=12)
-    capacity: int
 
 
 app = FastAPI()
